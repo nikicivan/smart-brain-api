ddda3a8b08f379a1e53986eef7c5dfdebf0d42ab
nikicivan <ivan.nikic@gmail.com>
2020-04-22 00:08:13 +0200

    updating

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const register = require('./Controllers/Register');
 const signin = require('./Controllers/Signin');
 const profile = require('./Controllers/Profile');
 const image = require('./Controllers/Image');
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
 	client: 'pg',
 	connection: {
 		connectionString: process.env.DATABASE_URL, //heroku host
-		ssl:true
+		ssl:true,
 	}
 });
 
