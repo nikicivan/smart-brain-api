031e066f0e484089ccb1a45821fb4d6f432a2e37
nikicivan <ivan.nikic@gmail.com>
2020-04-23 18:55:54 +0200

    updating

--- server.js
+++ server.js
@@ -17,22 +17,18 @@ const db = knex({
 	}
 });
 
-const router = express.Router();
-
-//db.connect();
-
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 
-app.get('/', (req, res) => {res.send('it is working!')});
-app.post('/signin', signin.handleSignin(db, bcrypt));
-app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt)});
-app.get('/profile/:id', (req,res) => {profile.handleProfile(req, res, db)});
-app.put('/image', (req,res) => {image.handleImage(req, res, db)});
-app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)});
+app.get('/', (req, res) => {res.send('it is working!')})
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3001, () => {
 	console.log(`app is running on port ${process.env.PORT}`);
